f6aeb40d80d9fdcb8ca61ad6619115ed29aa66c6
Rayan-Tarapore <ratitarapore@gmail.com>
2020-12-09 21:12:38 +0530

    HAH

--- sketch.js
+++ sketch.js
@@ -1,29 +1,49 @@
 var ball;
-
+var database,ballPos,databaseref
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    database = firebase.database();
+    databaseref = database.ref('Ball/Position');
+    databaseref.on("value",readPos,showErr);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-5,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(5,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-5);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+5);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('Ball/Position').set(
+       {
+            x:ball.x + x,
+            y:ball.y + y 
+        }
+    )
+  
+}
+
+function readPos(data){
+ballPos = data.val();
+ball.x = ballPos.x;
+ball.y = ballPos.y;
+
+}
+
+function showErr(){
+    console.log("ERROR")
 }
